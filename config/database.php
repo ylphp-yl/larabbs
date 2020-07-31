fe9f29f6f14d7476597990efe35e19281d27dcf9
ylphp <1083419457@qq.com>
2020-07-31 04:22:04 +0000

    翻译队列化

--- config/database.php
+++ config/database.php
@@ -129,7 +129,9 @@ return [
         'default' => [
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', null),
+            'parameters'=>[
+                'password'=>env('REDIS_PASSWORD', null),
+            ],
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_DB', '0'),
         ],
