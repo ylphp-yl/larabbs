e425d9dc6618b374649499dc5691b3011373f524
ylphp <1083419457@qq.com>
2020-08-01 02:36:25 +0000

    用户可以发表评论

--- routes/web.php
+++ routes/web.php
@@ -34,4 +34,4 @@ Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload
 
 //百度翻译seo有好的url
 Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
-Route::resource('replies', 'RepliesController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('replies', 'RepliesController', ['only' => [ 'store', 'destroy']]);
