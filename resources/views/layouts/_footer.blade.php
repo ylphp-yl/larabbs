d97b82bdb00c2717eea83b8dc5fa937b8a67c955
ylphp <1083419457@qq.com>
2020-08-02 14:22:43 +0000

    集成permission扩展包

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <div class="container">
         <p class="float-left">
-            由 <a href="http://weibo.com/u/1837553744?is_hot=1" target="_blank">叶亮</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+            由 <a href="http://weibo.com/u/1837553744?is_hot=1" target="_blank">亮仔</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
         </p>
 
         <p class="float-right"><a href="mailto:1083419457@qq.com">联系我</a></p>
