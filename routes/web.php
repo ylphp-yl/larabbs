0d47e41af8a06991917cf7d6ecb738f7be97b875
ylphp <1083419457@qq.com>
2020-08-02 19:10:27 +0000

    首页话题列表

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::get('/', 'PagesController@root')->name('root');
+Route::get('/', 'TopicsController@index')->name('root');
 Route::get('to_contact_me', 'PagesController@contact')->name('contact');
 // 用户身份验证相关的路由
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
