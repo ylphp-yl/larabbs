75a865e5b47967b87a9e3a29d101eaacab2d3fc4
ylphp <1083419457@qq.com>
2020-08-02 15:58:54 +0000

    站点设置

---
+++ resources/views/pages/founder.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+@section('title', '首页')
+
+@section('content')
+    <div class="ui centered grid container stackable mt-5 pt-3">
+        <div class="six wide column">
+            <div class="ui stacked segment">
+                <div class="content">
+
+                    <div class="text-center">
+
+                        <div class="ui message warning">
+
+                            <i class="icon info"></i>添加微信请道明来意，谢谢 ;-)
+                        </div>
+
+                        <img src="https://cdn.learnku.com/uploads/images/202003/20/1/evRoCOFIsQ.png!large" alt="" width="250" style="border: 2px solid #e4e4e4;border-radius: 4px;margin-bottom: 18px;">
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
